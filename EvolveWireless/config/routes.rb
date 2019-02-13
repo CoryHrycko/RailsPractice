5ac86672d50ca734fe88ca49869a5349b5e12a95
Cory Hrycko <chrycko9491@gmail.com>
2019-02-12 23:23:37 -0500

    updating this project

--- EvolveWireless/config/routes.rb
+++ EvolveWireless/config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'homes/index'
+  root 'homes#index' 
+
   direct :main do
-    { controller: 'home', action: 'index', subdomain: 'www' }
+    { controller: 'homes', action: 'index', subdomain: 'www' }
   end
+
 end
+
